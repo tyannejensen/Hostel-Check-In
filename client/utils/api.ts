cd048be781ce67faf57ab2077a30f2a47377577a
flynno <luke.laz.flynn@gmail.com>
2025-01-22 14:22:50 -0700

    application restart

--- client/utils/api.ts
+++
@@ -1,6 +0,0 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
-
-export async function fetchTenants() {
-    const res = await fetch(`${API_URL}/tenants`);
-    return res.json();
-}
