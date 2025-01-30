fac4ed0fec4436f2ba96f973bf9928e9988a2065
flynno <luke.laz.flynn@gmail.com>
2025-01-30 10:27:04 -0700

    moved auth actions to auth.actions.ts file

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -14,21 +14,20 @@ import { Label } from "@/components/ui/label"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 //Adding auth to the page
 import { useActionState } from "react"
-import { authenticate } from "@/lib/actions"
+import { authenticate } from "@/actions/auth.actions"
 import { useSearchParams } from "next/navigation"
-import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
+import { ExclamationCircleIcon } from "@heroicons/react/24/outline"
 
 export default function Page() {
-
-  const searchParams = useSearchParams()
-  const callBackUrl = searchParams?.get("callbackUrl") || "/dashboard/";
-  const [errorMessage, formAction, isPending] = useActionState(
-    authenticate, 
-    undefined,
-  );
+	const searchParams = useSearchParams()
+	const callBackUrl = searchParams?.get("callbackUrl") || "/dashboard/"
+	const [errorMessage, formAction, isPending] = useActionState(
+		authenticate,
+		undefined
+	)
 
 	return (
-<>
+		<>
 			<div id="grad">
 				<div id="text" className=" flex items-center justify-center h-screen">
 					<Tabs defaultValue="register" id="text" className="w-[400px] text">
@@ -74,69 +73,77 @@ export default function Page() {
 									</div>
 								</CardContent>
 								<CardFooter className="flex justify-end">
-									<Button id="contrast" className="drop-shadow-lg" aria-disabled={isPending}>
+									<Button
+										id="contrast"
+										className="drop-shadow-lg"
+										aria-disabled={isPending}
+									>
 										Submit
 									</Button>
-                  {errorMessage && (
-                    <>
-                      <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-                      <p className="text-sm text-red-500">{errorMessage}</p>
-                    </>
-                  )}
+									{errorMessage && (
+										<>
+											<ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+											<p className="text-sm text-red-500">{errorMessage}</p>
+										</>
+									)}
 								</CardFooter>
 							</Card>
 						</TabsContent>
 						<form action={formAction}>
-						<TabsContent value="login">
-							<Card>
-								<CardHeader>
-									<CardTitle id="text" className="font-bold text-xl">
-										Login
-									</CardTitle>
-									<CardDescription>
-										Welcome back! Please login to continue.
-									</CardDescription>
-								</CardHeader>
-								<CardContent id="text" className="space-y-2">
-									<div className="space-y-1">
-										<Label htmlFor="email">Email</Label>
-										<Input 
-										id="email" 
-										placeholder="Email" 
-										type="email" 
-										name="email"
-										required
+							<TabsContent value="login">
+								<Card>
+									<CardHeader>
+										<CardTitle id="text" className="font-bold text-xl">
+											Login
+										</CardTitle>
+										<CardDescription>
+											Welcome back! Please login to continue.
+										</CardDescription>
+									</CardHeader>
+									<CardContent id="text" className="space-y-2">
+										<div className="space-y-1">
+											<Label htmlFor="email">Email</Label>
+											<Input
+												id="email"
+												placeholder="Email"
+												type="email"
+												name="email"
+												required
+											/>
+										</div>
+										<div className="space-y-1">
+											<Label htmlFor="password">Password</Label>
+											<Input
+												id="password"
+												placeholder="Password"
+												type="password"
+												name="password"
+												required
+											/>
+										</div>
+									</CardContent>
+									<CardFooter className="flex justify-end">
+										<input
+											type="hidden"
+											name="redirectTo"
+											value={callBackUrl}
 										/>
-									</div>
-									<div className="space-y-1">
-										<Label htmlFor="password">Password</Label>
-										<Input
-											id="password"
-											placeholder="Password"
-											type="password"
-											name="password"
-											required
-										/>
-									</div>
-								</CardContent>
-                <CardFooter className="flex justify-end">
-                <input type="hidden" name="redirectTo" value={callBackUrl} />
-                  <Button id="dark-button" className="drop-shadow-lg">
-                    Login
-                  </Button>
-                  {errorMessage && (
-                    <>
-                      <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-                      <p className="text-sm text-red-500">{errorMessage}</p>
-                    </>
-                  )}
-                </CardFooter>
-              </Card>
-            </TabsContent>
-			</form>
-          </Tabs>
-        </div>
-      </div>
-    </>
-  );
+										<Button id="dark-button" className="drop-shadow-lg">
+											Login
+										</Button>
+										{errorMessage && (
+											<>
+												<ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+												<p className="text-sm text-red-500">{errorMessage}</p>
+											</>
+										)}
+									</CardFooter>
+								</Card>
+							</TabsContent>
+						</form>
+					</Tabs>
+				</div>
+			</div>
+		</>
+	)
 }
--- src/lib/data.ts
+++
@@ -1,9 +0,0 @@
-// TODO: Add all get functions (fetch) to this file
-
-export async function getReservationById(id: string) {
-	// TODO: fetch a reservation by id
-}
-
-export async function getTenantById(id: string) {
-	// TODO: fetch a tenant by id
-}
