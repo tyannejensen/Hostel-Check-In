bddb8f441a4f26f51cc205b54865890cd5202a01
flynno <luke.laz.flynn@gmail.com>
2025-01-30 10:15:19 -0700

    set up actions and data directories with new package.json files

---
+++ src/actions/user.actions.ts
@@ -0,0 +1,1 @@
+import { User } from "@/models/index" // Ensure this path is correct
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,25 +1,25 @@
 // TODO: add all mutation functions to this file.
 
-'use server';
+"use server"
 
-import { signIn } from '@/lib/auth';
-import { AuthError } from 'next-auth';
+import { signIn } from "@/lib/auth"
+import { AuthError } from "next-auth"
 
 export async function authenticate(
-    prevState: string | undefined,
-    formData: FormData,
+	prevState: string | undefined,
+	formData: FormData
 ) {
-    try {
-        await signIn('credentials', formData);
-        } catch (error) {
-            if (error instanceof AuthError) {
-                switch (error.type) {
-                    case 'CredentialsSignin':
-                        return 'Invalid credentials';
-                    default:
-                        return 'An error occurred';
-                }
-            }
-            throw error;
-    }
+	try {
+		await signIn("credentials", formData)
+	} catch (error) {
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case "CredentialsSignin":
+					return "Invalid credentials"
+				default:
+					return "An error occurred"
+			}
+		}
+		throw error
+	}
 }
