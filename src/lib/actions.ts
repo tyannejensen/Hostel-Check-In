f6abe61dbcb56349282c8321b9628bdfaa73127e
cjsquared-dev <camjohnson0618@gmail.com>
2025-01-30 11:32:33 -0700

    added signout functionality and button

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,8 +2,10 @@
 
 'use server';
 
-import { signIn } from '@/lib/auth';
+import { signIn, signOut } from '@/lib/auth';
 import { AuthError } from 'next-auth';
+import { dbConnect } from './db';
+import { User } from '@/models/User.model';
 
 export async function authenticate(
     prevState: string | undefined,
@@ -22,4 +24,36 @@ export async function authenticate(
             }
             throw error;
     }
+}
+
+export async function handleLogout() {
+    await signOut({ redirectTo: '/login' });
+  }
+
+export async function registerUser(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    await dbConnect();
+
+    const firstName = formData.get('firstName') as string;
+    const lastName = formData.get('lastName') as string;
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    if (!firstName || !lastName || !email || !password) {
+        return 'Please fill in all fields';
+    }
+
+    const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        password,
+        role: 'user',
+    });
+
+    await newUser.save();
+
+    return 'User created successfully';
 }
