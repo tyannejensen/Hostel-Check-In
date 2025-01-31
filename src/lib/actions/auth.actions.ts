327e1b21c499aa3bcf61504df29bd94264dc90ba
flynno <luke.laz.flynn@gmail.com>
2025-01-30 23:31:27 -0700

    move register user action from auth.actions to user.actions

--- src/lib/actions/auth.actions.ts
+++ src/lib/actions/auth.actions.ts
@@ -23,32 +23,3 @@ export async function authenticate(
 		throw error
 	}
 }
-
-// register user function
-export async function registerUser(
-	prevState: string | undefined,
-	formData: FormData
-) {
-	await dbConnect()
-
-	const firstName = formData.get("firstName") as string
-	const lastName = formData.get("lastName") as string
-	const email = formData.get("email") as string
-	const password = formData.get("password") as string
-
-	if (!firstName || !lastName || !email || !password) {
-		return "Please fill in all fields"
-	}
-
-	const newUser = new User({
-		firstName,
-		lastName,
-		email,
-		password,
-		role: "user",
-	})
-
-	await newUser.save()
-
-	return "User created successfully"
-}
