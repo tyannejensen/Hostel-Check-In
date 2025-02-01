5899518510c06d908a01b894ecc3638f0f02dd4a
flynno <luke.laz.flynn@gmail.com>
2025-01-31 22:29:41 -0700

    replaced "bookedBy" with "occupants" as an array in Room.model

--- src/lib/models/Room.model.ts
+++ src/lib/models/Room.model.ts
@@ -15,37 +15,68 @@ export const roomSchema = new Schema<IRoom>(
 			unique: [true, "Room number must be unique"],
 			validate: {
 				validator: (v: number | string) => {
-					return /^[0-9]{3}[a-zA-Z]$/.test(v.toString())
+					return /^(?:[1-9][0-9]{0,2}|[1-9][0-9]{0,2}[a-zA-Z])$/.test(
+						v.toString()
+					)
 				},
 				message: "{VALUE} is not a valid room number!",
 			},
-			status: {
-				type: String,
-				enum: ["available", "occupied", "maintenance", "cleaning"],
-				default: "available",
-			},
-			deposit: {
-				type: Number,
-				required: [true, "Deposit amount is required"],
-				get: (v: number) => v * 100, // Convert deposit amount to cents
-				set: (v: number) => v / 100, // Convert deposit amount to dollars
-			},
-			bookedBy: {
-				type: String,
-				ref: "User",
-				required: [true, "Tenant ID is required"],
-			},
-			name: {
-				type: String,
-				required: false,
-			},
-			occupants: {
-				type: Number,
-				required: false,
+		},
+		status: {
+			type: String,
+			enum: ["available", "occupied", "maintenance", "cleaning"],
+			default: "available",
+		},
+		deposit: {
+			type: Number,
+			required: [true, "Deposit amount is required"],
+			get: (v: number) => v * 100, // Convert deposit amount to cents
+			set: (v: number) => v / 100, // Convert deposit amount to dollars
+		},
+		name: {
+			type: String,
+			required: false,
+		},
+		size: {
+			type: Number,
+			required: [true, "Room size is required"],
+		},
+		occupants: {
+			type: [String], // Array of User IDs (uuid v4)
+			ref: "User",
+			required: true,
+			default: [],
+			validate: {
+				validator: function (this: IRoom) {
+					const count = this.occupants?.length || 0
+					// Check if occupants array is less than room size
+					return count <= this.size
+				},
+				message: "Room is full. Cannot add more occupants",
 			},
 		},
 	},
 	{ timestamps: true }
 )
+
+// SETTERS
+// Convert `_id` to string automatically when calling `.toJSON()`
+roomSchema.set("toJSON", {
+	virtuals: true,
+	getters: true,
+	transform: (doc, ret) => {
+		delete ret._id
+		return ret
+	},
+})
+// Convert `_id` to string automatically when calling `.toObject()`
+roomSchema.set("toObject", {
+	virtuals: true,
+	getters: true,
+	transform: (doc, ret) => {
+		delete ret._id
+		return ret
+	},
+})
 
 export const Room = models.Room || model<IRoom>("Room", roomSchema)
