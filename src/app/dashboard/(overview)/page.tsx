8f2b1fde1767c7fbcd094957a4343a828e967d09
Tyanne Jensen <vibewithme1123@Tyannes-Laptop.local>
2025-02-01 13:46:55 -0700

    fixed some run bugs

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,4 +1,4 @@
-import Reactfrom "react";
+import React from "react";
 import "@/styles/global.css";
 import { BedDouble, CircleSlash2, CalendarCheck2 } from "lucide-react";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
@@ -77,40 +77,40 @@ export default async function Page() {
           </CardContent>
         </Card>
 
-				<Card className="card">
-					<CardTitle>
-						<div className="card-decoration-light flex items-center justify-center">
-							<CircleSlash2 className="icon-dark" />
-						</div>
-					</CardTitle>
-					<CardContent>
-						<div>
-							<div>
-								<h3>Vacant Rooms</h3>
-								<h1 className="text-[42px]">44</h1>
-							</div>
-							<div className="progress"></div>
-						</div>
-					</CardContent>
-				</Card>
+        <Card className="card">
+          <CardTitle>
+            <div className="card-decoration-light flex items-center justify-center">
+              <CircleSlash2 className="icon-dark" />
+            </div>
+          </CardTitle>
+          <CardContent>
+            <div>
+              <div>
+                <h3>Vacant Rooms</h3>
+                <h1 className="text-[42px]">44</h1>
+              </div>
+              <div className="progress"></div>
+            </div>
+          </CardContent>
+        </Card>
 
-				<Card className="card">
-					<CardTitle>
-						<div className="card-decoration-dark flex items-center justify-center">
-							<CalendarCheck2 className="icon-light" />
-						</div>
-					</CardTitle>
-					<CardContent>
-						<div>
-							<div>
-								<h3>Rooms to Check-Out</h3>
-								<h1 className="text-[42px]">5</h1>
-							</div>
-							<div className="progress"></div>
-						</div>
-					</CardContent>
-				</Card>
-			</div>
+        <Card className="card">
+          <CardTitle>
+            <div className="card-decoration-dark flex items-center justify-center">
+              <CalendarCheck2 className="icon-light" />
+            </div>
+          </CardTitle>
+          <CardContent>
+            <div>
+              <div>
+                <h3>Rooms to Check-Out</h3>
+                <h1 className="text-[42px]">5</h1>
+              </div>
+              <div className="progress"></div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
 
       <div className="rounded-xl table-container border bg-card text-card-foreground shadow">
         <h1 className="pt-[10px] pb-[40px]">Tenants</h1>
