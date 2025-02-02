97f3dcb4e5a67576784371a440dc6dc0691a8651
flynno <luke.laz.flynn@gmail.com>
2025-02-01 21:36:14 -0700

    added server action getters for Tenants and Bookings (reservations) pages

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -3,7 +3,59 @@
 import { dbConnect } from "@/lib/db"
 import { User } from "@/models/User.model"
 
-// register user function
+// ----- GET DATA ----- //
+
+// Get all tenants
+export async function getTenants() {
+	try {
+		await dbConnect()
+
+		const tenants = await User.find({ role: "tenant" })
+			.populate("paymentMethods")
+			.populate("bookings")
+
+		if (!tenants)
+			return {
+				error: null,
+				message: "No tenants found",
+			}
+
+		const tenantObjs = tenants.map((tenant) => tenant.toObject())
+
+		return tenantObjs
+	} catch (error) {
+		console.error("Unable to get Tenants:", error)
+		return { error: error, message: "Unable to get Tenants" }
+	}
+}
+
+// Get tenant by ID
+export async function getTenantById(id: string) {
+	try {
+		await dbConnect()
+
+		const tenant = await User.findById(id)
+			.populate("paymentMethods")
+			.populate("bookings")
+
+		if (!tenant)
+			return {
+				error: null,
+				message: "Tenant not found",
+			}
+
+		const tenantObj = tenant?.toObject()
+
+		return tenantObj
+	} catch (error) {
+		console.error("Unable to get Tenant:", error)
+		return { error: error, message: "Unable to get Tenant" }
+	}
+}
+
+// ----- SET DATA ----- //
+
+// Register a new user
 export async function registerUser(
 	prevState: string | undefined,
 	formData: FormData
